098cecaed51d8b294ddbc2facaa8b89f590cc681
Jacob Hollis <jbhollis@pm.me>
2023-05-22 17:01:22 -0500

    Nav changes

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,26 @@
 <template>
   <v-app>
-    <v-bottom-navigation
-      v-model="value"
-      active
-      grow
-    >
-    <v-btn value="home">
-        <v-icon>mdi-home-circle-outline</v-icon>
-        Home
-      </v-btn>
-      <v-btn value="inventory">
-        <v-icon>mdi-package-variant</v-icon>
-        Inventory
-      </v-btn>
-      <v-btn value="metrics">
-        <v-icon>mdi-chart-line</v-icon>
-        Metrics
-      </v-btn>
-      <v-btn value="about">
-        <v-icon>mdi-help</v-icon>
-        About
-      </v-btn>
-    </v-bottom-navigation>
+    <NavigationView/>
     <v-main>
-      <HelloWorld/>
+      <HomeView/>
     </v-main>
-    <v-btn @click="toggleTheme">toggle theme</v-btn>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import { useTheme } from 'vuetify'
+import HomeView from './views/HomeView.vue'
+import NavigationView from './components/NavigationView.vue'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    HomeView: HomeView,
+    NavigationView: NavigationView
   },
 
   data: () => ({
     //
   }),
-
-  setup () {
-    const theme = useTheme()
-
-    return {
-      theme,
-      toggleTheme: () => theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
-    }
-  },
 }
 </script>
---
+++ src/components/NavigationView.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-bottom-navigation
+      v-model="value"
+      active
+      grow
+    >
+    <v-btn value="home" to="/">
+        <v-icon>mdi-home-circle-outline</v-icon>
+        Home
+      </v-btn>
+      <v-btn value="inventory" to="/inventory">
+        <v-icon>mdi-package-variant</v-icon>
+        Inventory
+      </v-btn>
+      <v-btn value="metrics" to="/metrics">
+        <v-icon>mdi-chart-line</v-icon>
+        Metrics
+      </v-btn>
+      <v-btn value="about" to="/about">
+        <v-icon>mdi-help</v-icon>
+        About
+      </v-btn>
+    </v-bottom-navigation>
+    <v-col class="text-right">
+        <v-btn @click="toggleTheme">
+            <v-icon>mdi-brightness-4</v-icon>
+        </v-btn>
+    </v-col>
+</template>
+
+<script>
+import { useTheme } from 'vuetify'
+
+export default {
+    name: 'NavigationView',
+
+    setup () {
+    const theme = useTheme()
+
+    return {
+      theme,
+      toggleTheme: () => theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+    }
+  },
+};
+</script>
+
+<style scoped>
+</style>
